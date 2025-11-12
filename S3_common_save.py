a6d6aa59b78641b9b90d4bcd2316e57e5801f60f
fuwafuwarx782-cmyk <fuwafuwarx782@gmail.com>
2025-11-12 15:08:43 +0900

    更新

--- S3_common_save.py
+++ S3_common_save.py
@@ -5,13 +5,13 @@ from datetime import datetime
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 S3_PREFIX = "cpi_raw_data" #プレフィックスはデータを取得する辞書のキーかつフォルダみたいなもの
 
-def save_to_s3(csv_string: str, file_name: str):
+def S3_common_save(csv_string: str, file_name: str):
 
     if not S3_BUCKET_NAME:
         raise ValueError("S3_BUCKET_NAMEが環境変数に設定されていません！")
         
     try:
-        s3 = boto3.client('s3')
+        s3 = boto3.client('s3') #接続開始
         s3_key = f"{S3_PREFIX}/{file_name}" #ファイルのパスを指定
         
         s3.put_object(
